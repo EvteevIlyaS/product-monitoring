222a9e9cd2e67a8a064151db9f9d91d03b4f0f00
Ilya Evteev <evteev-ilya@inbox.ru>
2023-05-15 19:16:47 +0300

    edit: fix password/email change - now demands old password

--- src/test/java/com/ilyaevteev/productmonitoring/service/impl/UserServiceImplTest.java
+++ src/test/java/com/ilyaevteev/productmonitoring/service/impl/UserServiceImplTest.java
@@ -13,7 +13,6 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import java.util.Optional;
