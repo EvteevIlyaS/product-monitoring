02b3d2cad111c4ccbf77e388cb648f73fc1799f5
Ilya Evteev <evteev-ilya@inbox.ru>
2023-05-30 14:22:53 +0300

    fix: swagger security configuration

---
+++ src/main/java/com/ilyaevteev/productmonitoring/config/SwaggerConfig.java
@@ -0,0 +1,24 @@
+package com.ilyaevteev.productmonitoring.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("product-monitoring").version("0.0.1"))
+                .components(new Components()
+                        .addSecuritySchemes("JWTSecretHeader", new SecurityScheme()
+                                .type(SecurityScheme.Type.APIKEY)
+                                .in(SecurityScheme.In.HEADER)
+                                .name("Authorization")))
+                .addSecurityItem(new SecurityRequirement().addList("JWTSecretHeader"));
+    }
+}
